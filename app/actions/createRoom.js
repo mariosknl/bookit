2b32cb2de58fb2256e9b679f876436d68a90c4a8
mariosknl <mariosknl@outlook.com>
2024-10-10 14:28:35 +0300

    storage buckets & image upload

--- app/actions/createRoom.js
+++ app/actions/createRoom.js
@@ -17,6 +17,26 @@ async function createRoom(previousState, formData) {
       };
     }
 
+    // Uploading Image
+    let imageID;
+
+    const image = formData.get("image");
+
+    if (image && image.size > 0 && image.name !== "undefined") {
+      try {
+        // Upload
+        const response = await storage.createFile("rooms", ID.unique(), image);
+        imageID = response.$id;
+      } catch (error) {
+        console.log("Error uploading image", error);
+        return {
+          error: "Error uploading image",
+        };
+      }
+    } else {
+      console.log("No image file provided or file is invalid");
+    }
+
     // Create room
     const newRoom = await databases.createDocument(
       process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
@@ -33,7 +53,7 @@ async function createRoom(previousState, formData) {
         availability: formData.get("availability"),
         price_per_hour: formData.get("price_per_hour"),
         amenities: formData.get("amenities"),
-        // image: imageID,
+        image: imageID,
       },
     );
 
