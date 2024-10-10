b5c17bfa9dd8ffbd4b309e7255ae12eafe32807d
mariosknl <mariosknl@outlook.com>
2024-10-10 16:56:42 +0300

    booking form, show bookings, bookedRoomCard component, cancel booking

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,7 +1,20 @@
-const BookingsPage = () => {
+import Heading from "@/components/Heading";
+import BookedRoomCard from "@/components/BookedRoomCard";
+import getMyBookings from "../actions/getMyBookings";
+
+const BookingsPage = async () => {
+  const bookings = await getMyBookings();
+
   return (
     <>
-      <h1>Bookings</h1>
+      <Heading title="My Bookings" />
+      {bookings.length === 0 ? (
+        <p className="text-gray-600 mt-4">You have no bookings</p>
+      ) : (
+        bookings.map((booking) => (
+          <BookedRoomCard key={booking.$id} booking={booking} />
+        ))
+      )}
     </>
   );
 };
